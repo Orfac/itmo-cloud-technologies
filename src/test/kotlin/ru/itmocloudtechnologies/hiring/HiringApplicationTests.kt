6a42b2cf85d63181aee21555a8e05c7b7a81306d
Arseniy Molodetskiy <molodeckij.b.2017@gmail.com>
2022-05-06 18:13:54 +0300

    добавил базовую конфигурацию проекта

---
+++ src/test/kotlin/ru/itmocloudtechnologies/hiring/HiringApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.itmocloudtechnologies.hiring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HiringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

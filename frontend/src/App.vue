40ed68dc4c955f66eb1c5261517053903c9222e7
Andrew Belyy <andrew151612@gmail.com>
2022-05-09 01:28:35 +0300

    добавил конфигурацию фронта

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+.app 
+    .app__title hello world frontend
+</template>
+
+<style lang="sass">
+body
+  margin: 0px
+  padding: 0px
+.app
+  &__title
+    text-align: center
+    margin-top: 10px
+</style>

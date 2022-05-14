bb377bd92ac9ffb5fd63b29c222e7db6204b9197
Arseniy Molodetskiy <molodeckij.b.2017@gmail.com>
2022-05-14 03:28:09 +0300

    добавил CORS

---
+++ src/main/kotlin/ru/itmocloudtechnologies/hiring/configuration/CorsConfiguration.kt
@@ -0,0 +1,28 @@
+package ru.itmocloudtechnologies.hiring.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+class CorsFilterConfiguration {
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("*") // this allows all origin
+        config.addAllowedHeader("*") // this allows all headers
+        config.addAllowedMethod("OPTIONS")
+        config.addAllowedMethod("HEAD")
+        config.addAllowedMethod("GET")
+        config.addAllowedMethod("PUT")
+        config.addAllowedMethod("POST")
+        config.addAllowedMethod("DELETE")
+        config.addAllowedMethod("PATCH")
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
+    }
+}

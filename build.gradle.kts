898672b87065baa4b3b4be61dcf3726ea43532b4
Arseniy Molodetskiy <molodeckij.b.2017@gmail.com>
2022-05-14 03:28:54 +0300

    добавление openapi

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springdoc:springdoc-openapi-ui:1.6.8")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
